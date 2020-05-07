1463fe44a06637c16955326de9e75868225c6cdd
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-07 14:40:03 -0400

    Pipes Aynsc y Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +13,8 @@ export class AppComponent {
   PI: number;
   porcentaje: number;
   salario: number;
+  objeto: any;
+  valorPromesa: any;
 
   constructor() {
     this.nombre = 'Enrique';
@@ -19,5 +22,21 @@ export class AppComponent {
     this.PI = Math.PI;
     this.porcentaje = 0.234;
     this.salario =  12345;
+    this.objeto = {
+      nombre: 'Enrique',
+      apellido: 'Villasana',
+      edad: 27,
+      direccion: {
+        urb: 'lomas de la Hacienda',
+        condominio: 'Pinar 5',
+        casa: 24
+      }
+    };
+
+    this.valorPromesa = new Promise<string>( (resolve) => {
+      setTimeout(() => {
+        resolve('Llegó la data');
+      }, 4500);
+    } );
   }
 }
